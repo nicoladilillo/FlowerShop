c6119d321d4869bae8763c6c19cf63fbfdcfbc6d
Nicola Dilillo <nicoladilillo98@gmail.com>
2017-04-18 12:32:54 +0200

    Add signup

---
+++ sign.php
@@ -0,0 +1,28 @@
+<?php
+  include 'function.php';
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $confirmation = $_POST['confirmation'];
+  $name = $_POST['name'];
+  $surname = $_POST['surname'];
+  $email = $_POST['email'];
+
+  $data = new User;
+  $data->connectMysql();
+
+  /******** CHECK SAME PASSWORD *******/
+  if( $password !== $confirmation ) {
+    echo "Please insert equal passwords.";
+    exit;
+  }
+
+  /********** CHECK USERNAME *********/
+  if( $data->isRegistered($username) ) {
+    echo "Username already exists.";
+    exit;
+  }
+
+  /* IF EVERYTHING IS OKAY, STORE THE NEW USER INFORMATIONS */
+  $data->signUp($email, $password, $name, $surname, $username);
+  echo "Successfully registered.";
---
+++ signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Sign up</title>
+  </head>
+
+  <body>
+    <div class="signup">
+      <form action="sign.php" method="post">
+        <input name="username" type="text" placeholder="username" />
+        <input name="password" type="password" placeholder="password" />
+        <input name="confirmation" type="password" placeholder="confirmation password" />
+        <input name="name" type="text" placeholder="name" />
+        <input name="surname" type="text" placeholder="surname" />
+        <input name="email" type="email" placeholder="email" />
+        <div>
+            <input type="submit" value="Sign Up" />
+            <input type="reset" value="Reset" />
+        </div>
+      </form>
+    </div>
+
+  </body>
+</html>
