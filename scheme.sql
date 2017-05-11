8af9958ffdfee7bfc281810406c706e5607c031f
Nicola Dilillo <nicoladilillo98@gmail.com>
2017-05-12 00:10:35 +0200

    Change scheme

--- scheme.sql
+++ scheme.sql
@@ -58,10 +58,18 @@ CREATE TABLE `flower` (
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `flower`
+--
 
+LOCK TABLES `flower` WRITE;
+/*!40000 ALTER TABLE `flower` DISABLE KEYS */;
+INSERT INTO `flower` VALUES (1,'Array','Array','Array','Array','Array','Array','Array','Array'),(2,'Array','Array','Array','Array','Array','Array','Array','Array'),(3,'Array','Array','Array','Array','Array','Array','Array','Array'),(4,'Array','Array','Array','Array','Array','Array','Array','Array'),(5,'Array','Array','Array','Array','Array','Array','Array','Array'),(6,'name','city','address','telefon','owner','username','email','password'),(7,'123','Barletta','via Roma','123','Nicola Dilillo','nicola','nicola@gmail.com','123'),(8,'f','f','f','f','f','f','f@f','1'),(9,'','','','','','','',''),(10,'Ni','d','q','w','w','nicola2','ds@gam','123'),(11,'q','q','q','q','q','r','w@w','q'),(12,'q','q','q','q','q','g','q@q','1'),(13,'d','d','d','d','d','d','d@d','d'),(14,'d','d','d','d','d','dd','d@d','d'),(15,'g','g','g','g','g','gw','g@g','g');
+/*!40000 ALTER TABLE `flower` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `product`
@@ -79,9 +87,47 @@ CREATE TABLE `product` (
   PRIMARY KEY (`ID`),
   KEY `id_fioraio` (`id_flower`),
   CONSTRAINT `product_ibfk_1` FOREIGN KEY (`id_flower`) REFERENCES `flower` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (14,'Margerita',2.5,7,'Molto bella');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchase`
+--
+
+DROP TABLE IF EXISTS `purchase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `purchase` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Date` date DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_product` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `id_user` (`id_user`),
+  KEY `id_product` (`id_product`),
+  CONSTRAINT `purchase_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`ID`),
+  CONSTRAINT `purchase_ibfk_2` FOREIGN KEY (`id_product`) REFERENCES `product` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase`
+--
+
+LOCK TABLES `purchase` WRITE;
+/*!40000 ALTER TABLE `purchase` DISABLE KEYS */;
+/*!40000 ALTER TABLE `purchase` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -101,6 +147,15 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (5,'nicola','Nicola','Dilillo','nicola@gmail.com',123),(6,'ugo','ugo','costa','ugoi@gmail.com',123),(7,'ugo2','ugo','costa','ugoi@gmail.com',123);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -111,4 +166,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-02 17:10:03
+-- Dump completed on 2017-05-11 22:09:17
