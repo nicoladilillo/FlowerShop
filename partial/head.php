5881a4b15665ad7efb92f3e77bc625d81d8abf1c
Nicola Dilillo <nicoladilillo98@gmail.com>
2017-05-16 00:14:27 +0200

    Fix other links

--- partial/head.php
+++ partial/head.php
@@ -10,8 +10,8 @@
   <head>
     <meta charset="utf-8">
     <title><?php echo($type) ?></title>
-    <link rel="icon" href="/assets/img/flower-icon.png">
-    <script src="/vendor/components/jquery/dist/jquery.js"></script>
+    <link rel="icon" href="./assets/img/flower-icon.png">
+    <script src="./vendor/components/jquery/jquery.js"></script>
   </head>
   <body>
     Come back...<button type=button onclick="window.open('../', '_self')">Home</button><br />
