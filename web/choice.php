26ceeaebf86957f3882b8d3c4554e4f523429efb
Nicola Dilillo <nicoladilillo98@gmail.com>
2017-05-16 00:56:09 +0200

    Fix Structure

--- web/choice.php
+++
@@ -1,4 +0,0 @@
-<button value="administrator">administrator</button>
-<button value="flower">flower</button>
-<button value="user">user</button>
-<script src="assets/js/choice.js"></script>
--- web/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-  class Config {
-      public $servername = 'localhost';
-      public $username = 'vagrant';
-      public $password = 'vagrant';
-      public $dbname = 'fioraio';
-  }
