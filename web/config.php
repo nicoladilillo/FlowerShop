b36a7e6b87bd66d9133cfd8368d6aeb0d942824a
Nicola Dilillo <nicoladilillo98@gmail.com>
2017-05-16 18:15:56 +0200

    Fix

--- web/config.php
+++ web/config.php
@@ -5,11 +5,12 @@
       public $username;
       public $password;
       public $dbname;
+      private $inf = getenv("CLEARDB_DATABASE_URL");
 
       #constructor
       function __construct() {
 
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $url = parse_url($this->inf);
 
         $this->$servername = $url["host"];
         $this->$username = $url["user"];
